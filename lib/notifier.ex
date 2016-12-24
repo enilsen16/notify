bd2e6386cfc588ef91ca93dd1effb5065c61b795
Erik Nilsen <enilsen16@live.com>
2016-12-24 15:28:11 -0800

    ecto and postgrex

---
+++ lib/notifier.ex
@@ -0,0 +1,18 @@
+defmodule Notifier do
+  @moduledoc """
+  Documentation for Notifier.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Notifier.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
